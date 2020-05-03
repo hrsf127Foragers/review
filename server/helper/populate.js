764367d5007f49abe6b0d5d9e30d26fb5af3be4e
Charlie Thao <cthao309@gmail.com>
2020-05-03 01:52:04 -0700

    refactored populate.js to object methods

--- server/helper/populate.js
+++ server/helper/populate.js
@@ -1,176 +1,196 @@
 const mysql = require('mysql');
 const connection = require('../../db/config.js');
-const data = require('./seedData.js');
+const method = require('./seedData.js');
 
-// declaration of data to be generate
-let restaurantDataLimt = 100;
-let userDataLimt = 100;
-let postDataLimt = 100;
-let postImageDataLimt = 100;
+const randomGeneratedMethod = {
+  // declaration of data to be generate
+  restaurantDataLimt: 100,
+  userDataLimt: 100,
+  postDataLimt: 200,
+  postImageDataLimt: 200,
 
-// declaration of restaurant information
-const restaurantObj = {};
+  // declaration of restaurant information
+  restaurantObj: {},
 
-// function to randomly generate 100 restaurant's name
-const getRestaurants = () => {
-  let restaurants = [];
+  // function to randomly generate 100 restaurant's name
+  generateRestaurants: () => {
+    let restaurants = [];
 
-  for (var i = 1; i <= restaurantDataLimt; i++) {
-    restaurants.push(data.generateRestaurant(i));
+    for (var i = 1; i <= randomGeneratedMethod.restaurantDataLimt; i++) {
+      restaurants.push(method.generateRestaurant(i));
+    }
+
+    randomGeneratedMethod.restaurantObj["restaurants"] = restaurants;
+  },
+
+  // function to randomly generate 100 users
+  generateUsers: () => {
+    let users = [];
+
+    for(let i = 1; i <= randomGeneratedMethod.userDataLimt; i++) {
+      users.push(method.generateUser(i));
+    }
+
+    randomGeneratedMethod.restaurantObj["users"] = users;
+  },
+
+  // function to generate post
+  generatePost: () => {
+    randomGeneratedMethod.restaurantObj["posts"] = method.generatePost(randomGeneratedMethod.postDataLimt, randomGeneratedMethod.restaurantObj.restaurants, randomGeneratedMethod.restaurantObj.users);
+  },
+
+  // function to generate post images
+  generatePostImage: () => {
+    randomGeneratedMethod.restaurantObj["post_images"] = method.generateImage(randomGeneratedMethod.postImageDataLimt);
+  },
+
+  // function to create the relationship between restaurant, post and user
+  generateRelationship: () => {
+    let relationTable = method.createRelation(randomGeneratedMethod.restaurantObj.restaurants, randomGeneratedMethod.restaurantObj.users, randomGeneratedMethod.restaurantObj.posts)
+
+    randomGeneratedMethod.restaurantObj["relationTable"] = relationTable;
   }
 
-  restaurantObj["restaurants"] = restaurants;
-};
-
-// function to randomly generate 100 users
-const getUsers = () => {
-  let users = [];
-
-  for(let i = 1; i <= userDataLimt; i++) {
-    users.push(data.generateUser(i));
-  }
-
-  restaurantObj["users"] = users;
 }
 
-// function to generate post
-function getPost() {
-  restaurantObj["posts"] = data.generatePost(postDataLimt, restaurantObj.restaurants, restaurantObj.users);
-}
+randomGeneratedMethod.generateRestaurants();
+randomGeneratedMethod.generateUsers();
+randomGeneratedMethod.generatePost();
+randomGeneratedMethod.generatePostImage();
+randomGeneratedMethod.generateRelationship();
 
-// function to generate post images
-function getPostImage() {
-  restaurantObj["post_images"] = data.generateImage(postImageDataLimt);
-}
+let restaurantData = randomGeneratedMethod.restaurantObj;
+// console.log(restaurantData.posts)
 
-// function to create the relationship between restaurant, post and user
-function createRelationship() {
-  let relationTable = data.createRelation(restaurantObj.restaurants, restaurantObj.users, restaurantObj.posts)
-
-  restaurantObj["relationTable"] = relationTable;
-}
-
-getRestaurants();
-getUsers();
-getPost();
-getPostImage();
-createRelationship();
-
-
-// console.log(restaurantObj)
 
 // ############################################################################################
 // POPULATE THE DATABASE WITH THE DUMMY DATA
 // ############################################################################################
+const databaseMethods = {
+  // function to insert all the restaurant to mysql
+  insertRestaurants: (restaurant, callback) => {
+    let restaurantName = restaurant.storeName;
 
-// function to insert all the restaurant to mysql
-const insertRestaurants = (restaurant, callback) => {
-  let restaurantName = restaurant.storeName;
+    let queryStr = `INSERT INTO restaurants (restaurant_name) VALUES ("${restaurantName}")`;
 
-  let queryStr = `INSERT INTO restaurants (restaurant_name) VALUES ("${restaurantName}")`;
+    connection.query(queryStr, (err, res) => {
+      if(err) {
+        console.log('error saving..')
+        callback(err, null)
+      } else {
+        callback(null, 'Success')
+      }
+    });
+  },
 
-  connection.query(queryStr, (err, res) => {
-    if(err) {
-      console.log('error saving..')
-      callback(err)
-    } else {
-      callback('Success')
+  // let restaurantData = restaurantObj.restaurants;
+
+  // restaurantLoop function
+  restaurantLoop: (restaurants) => {
+    for(let i = 0 ; i < restaurants.length; i++) {
+      databaseMethods.insertRestaurants(restaurants[i], (err, res) => {
+        if(err) {
+          console.log('Error saving', err)
+        } else {
+          console.log('Successfully save restaurants: ', i)
+        }
+      })
     }
-  });
+  },
+
+  // function to insert all the users to mysql
+  insertUsers: (user, callback) => {
+    let queryStr = `INSERT INTO users (user_name, location, friends, reviews, photos) VALUES (?, ?, ?, ?, ?)`;
+
+    connection.query(queryStr, [user.user_name, user.location, user.friends, user.reviews, user.photos], (err, res) => {
+      if(err) {
+        console.log('error saving..')
+        callback(err, null)
+      } else {
+        callback(null, 'Success')
+      }
+    });
+  },
+
+  // let userData = restaurantObj.users;
+
+  // userLoop function
+  userLoop: (users) => {
+    for(let i = 0 ; i < users.length; i++) {
+      databaseMethods.insertUsers(users[i], (err, res) => {
+        if(err) {
+          console.log('Error saving', err)
+        } else {
+          console.log('Successfully save users: ', i)
+        }
+      })
+    }
+  },
+
+  // function to insert all the posts to mysql
+  insertPosts: (post, callback) => {
+    let queryStr = `INSERT INTO posts (rating, created_at, check_in, useful, funny, cool, post, user_id, restaurant_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+
+    connection.query(queryStr, [post.rating, post.created_at, post.check_in, post.useful, post.funny, post.cool, post.post,post.user_id, post.restaurant_id], (err, res) => {
+      if(err) {
+        callback(err, null)
+      } else {
+        callback(null, res)
+      }
+    });
+  },
+
+  // let postData = restaurantObj.posts;
+
+  // postLoop function
+  postLoop: (posts) => {
+    for(let i = 0 ; i < posts.length; i++) {
+      databaseMethods.insertPosts(posts[i], (err, res) => {
+        if(err) {
+          console.log('Error saving', err)
+        } else {
+          console.log('Successfully save posts:', i)
+        }
+      })
+    }
+  },
+
+
+  // function to insert all the posts to mysql
+  insertPostImages: (image, callback) => {
+    let queryStr = `INSERT INTO post_images (post_id, image_url) VALUES (?, ?)`;
+
+    connection.query(queryStr, [image.post_id, image.image_url], (err, res) => {
+      if(err) {
+        callback(err, null)
+      } else {
+        callback(null, res)
+      }
+    });
+
+  },
+
+  // let postImageData = restaurantObj.post_images;
+
+  // postLoop function
+  postImageLoop: (postImage) => {
+    for(let i = 0 ; i < postImage.length; i++) {
+      databaseMethods.insertPostImages(postImage[i], (err, res) => {
+        if(err) {
+          console.log('Error saving', err)
+        } else {
+          console.log('Successfully save postImage: ', i)
+        }
+      })
+    }
+    return;
+  }
 }
 
-let restaurantData = restaurantObj.restaurants;
+let restaurantDetail = randomGeneratedMethod.restaurantObj;
 
-for(let i = 0 ; i < restaurantData.length; i++) {
-  insertRestaurants(restaurantData[i], (err, res) => {
-    if(err) {
-      console.log('Error saving', err)
-    } else {
-      console.log('Successfully save')
-    }
-  })
-}
+databaseMethods.restaurantLoop(restaurantDetail.restaurants);
+databaseMethods.userLoop(restaurantDetail.users);
+databaseMethods.postLoop(restaurantDetail.posts);
+databaseMethods.postImageLoop(restaurantDetail.post_images);
 
-// function to insert all the users to mysql
-const insertUsers = (user, callback) => {
-  let queryStr = `INSERT INTO users (user_name, location, friends, reviews, photos) VALUES (?, ?, ?, ?, ?)`;
-
-  connection.query(queryStr, [user.user_name, user.location, user.friends, user.reviews, user.photos], (err, res) => {
-    if(err) {
-      console.log('error saving..')
-      callback(err, null)
-    } else {
-      callback(null, 'Success')
-    }
-  });
-
-}
-
-let userData = restaurantObj.users;
-
-for(let i = 0 ; i < userData.length; i++) {
-  insertUsers(userData[i], (err, res) => {
-    if(err) {
-      console.log('Error saving', err)
-    } else {
-      console.log('Successfully save')
-    }
-  })
-}
-
-// function to insert all the posts to mysql
-const insertPosts = (post, callback) => {
-  // console.log('testing => ', post)
-
-  let queryStr = `INSERT INTO posts (rating, created_at, check_in, useful, funny, cool, post, user_id, restaurant_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
-
-  connection.query(queryStr, [post.rating, post.created_at, post.check_in, post.useful, post.funny, post.cool, post.post,post.user_id, post.restaurant_id], (err, res) => {
-    if(err) {
-      callback(err, null)
-    } else {
-      callback(null, res)
-    }
-  });
-
-}
-
-let postData = restaurantObj.posts;
-
-for(let i = 0 ; i < postData.length; i++) {
-  insertPosts(postData[i], (err, res) => {
-    if(err) {
-      console.log('Error saving', err)
-    } else {
-      console.log('Successfully save')
-    }
-  })
-}
-
-
-// function to insert all the posts to mysql
-const insertPostImages = (image, callback) => {
-  console.log('testing => ', image)
-
-  let queryStr = `INSERT INTO post_images (post_id, image_url) VALUES (?, ?)`;
-
-  connection.query(queryStr, [image.post_id, image.image_url], (err, res) => {
-    if(err) {
-      callback(err, null)
-    } else {
-      callback(null, res)
-    }
-  });
-
-}
-
-let postImageData = restaurantObj.post_images;
-
-for(let i = 0 ; i < postImageData.length; i++) {
-  insertPostImages(postImageData[i], (err, res) => {
-    if(err) {
-      console.log('Error saving', err)
-    } else {
-      console.log('Successfully save')
-    }
-  })
-}
