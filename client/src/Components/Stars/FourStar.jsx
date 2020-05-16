b034b1a963a01a93d0a40e396ddd06cfd5fd03e1
Charlie Thao <cthao309@gmail.com>
2020-05-16 14:41:42 -0700

    save latest change

--- client/src/Components/Stars/FourStar.jsx
+++ client/src/Components/Stars/FourStar.jsx
@@ -12,7 +12,7 @@ const FourStar = ({createdData}) => {
         <span className={styles.review_stars}> <i className="fa fa-star" aria-hidden="true"></i> </span>
         <span className={styles.empty_fill}> <i className="fa fa-star" aria-hidden="true"></i> </span>
       </div>
-      <div className={styles.created_date}>{{createdData}}</div>
+      <div className={styles.created_date}>{createdData}</div>
     </div>
   )
 }
